95350f6b3dd198ee8eb167b4ca91aa7b327cfd72
EDSONARANGUREN <edsonaranguren@gmail.com>
2021-05-01 22:12:19 +0000

    11

--- Employees/webapp/controller/MasterEmployee.controller.js
+++ Employees/webapp/controller/MasterEmployee.controller.js
@@ -13,26 +13,7 @@ sap.ui.define([
         "use strict";
 
         function onInit() {
-            var oView = this.getView();
-            //var i18nBundle = oView.getModel("i18n").getResourceBundle();
-
-            var oJSONModelEmployees = new sap.ui.model.json.JSONModel();
-            oJSONModelEmployees.loadData("./localService/mockdata/Employees.json", false);
-            oView.setModel(oJSONModelEmployees, "jsonEmployees");
-
-            var oJSONModelCountries = new sap.ui.model.json.JSONModel();
-            oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
-            oView.setModel(oJSONModelCountries, "jsonCountries");
-
-            var oJSONModelConfig = new sap.ui.model.json.JSONModel({
-                visibleID: true,
-                visibleName: true,
-                visibleCountry: true,
-                visibleCity: false,
-                visibleBtnShowCity: true,
-                visibleBtnHideCity: false
-            });
-            oView.setModel(oJSONModelConfig, "oJSONModelConfig");
+            this._bus = sap.ui.getCore().getEventBus();
         };
 
         function onFilter() {
@@ -103,7 +84,14 @@ sap.ui.define([
             this._oDialogOrders.close();
         };
 
-        var Main = Controller.extend("logaligroup.Employees.controller.mainView", {});
+        function showEmployee(oEvent) {
+            var path = oEvent.getSource().getBindingContext("jsonEmployees").getPath();
+            this._bus.publish("flexible", "showEmployee", path);
+
+        };
+
+
+        var Main = Controller.extend("logaligroup.Employees.controller.MasterEmployee", {});
 
         Main.prototype.onValidate = function () {
             var inputEmployee = this.byId("inputEmployee");
@@ -127,5 +115,6 @@ sap.ui.define([
         Main.prototype.onHideCity = onHideCity;
         Main.prototype.showOrders = showOrders;
         Main.prototype.onCloseOrders = onCloseOrders;
+        Main.prototype.showEmployee = showEmployee;
         return Main;
     });
